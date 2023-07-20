e2dabc085a5741a69579d775f64973250754d779
xshady <fastik036@gmail.com>
2023-07-20 17:20:35 +0300

    improve error

--- shared/src/remote-pending-controller/class.ts
+++ shared/src/remote-pending-controller/class.ts
@@ -32,7 +32,7 @@ export class RemotePendingController {
   private initTimer(): number {
     return setTimeout(() => {
       this.timer = null
-      this.reject(`rpc name: "${this.rpcName}" call duration was too long > ${this.timeout}`, ErrorCodes.Expired)
+      this.reject(`rpc name: "${this.rpcName}" call duration was too long > ${this.timeout} ms`, ErrorCodes.Expired)
     }, this.timeout)
   }
 
